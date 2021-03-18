a16e2e12d036b52f7f42b0dcbf5586ab86b05249
ravielze <ravielz20@gmail.com>
2021-03-19 01:55:06 +0700

    Menu

---
+++ HanyaPenggemar/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HanyaPenggemar
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string[] lines;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void Exit(object sender, RoutedEventArgs e)
+        {
+            App.Current.MainWindow.Close();
+        }
+
+        private void MoveWindow(object sender, MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+            this.DragMove();
+        }
+
+        private void OpenFile(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Multiselect = false,
+                Filter = "Text files (*.txt)|*.txt",
+                InitialDirectory = Directory.GetCurrentDirectory()
+            };
+            if (openFileDialog.ShowDialog() == true)
+            {
+                lines = System.IO.File.ReadAllLines(openFileDialog.FileName);
+                Paragraph paragraph = new Paragraph();
+                paragraph.Inlines.Add(System.IO.File.ReadAllText(openFileDialog.FileName));
+                FlowDocument document = new FlowDocument(paragraph);
+                Previewer.Document = document;
+            }
+        }
+
+        public string[] GetReadedFileLines()
+        {
+            return lines;
+        }
+    }
+}
